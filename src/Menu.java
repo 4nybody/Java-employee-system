db0487a023d6f92e6f0730dabadb8de548909146
4nybody <seanne.4barquez@gmail.com>
2025-09-17 18:02:21 +0800

    Add lib folder with required JARs

--- src/Menu.java
+++ src/Menu.java
@@ -2,6 +2,6 @@ public class Menu {
     public void show() {
         System.out.println("Menu displayed.");
         System.out.println("1. Add Employee");
-
+        System.out.println("2. Remove Employee");
     }
 }
